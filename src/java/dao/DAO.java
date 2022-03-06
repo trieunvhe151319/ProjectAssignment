bd56fe66dd0cf98ab1591b40493c18b3dfd1014d
trieunvhe151319 <97203188+trieunvhe151319@users.noreply.github.com>
2022-03-06 22:06:08 +0700

    Update
    Update categorycontrol, tạo detail.jsp để hiển thị chi tiết từng sản phẩm khi chọn.
    Chia home.jsp ra gồm footer, left, menu để dùng khi chọn vào detail.
    Update DAO thêm getbyCateID và getbyID

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -79,6 +79,47 @@ public class DAO {
         }
         return null;
     }
+      public List<Product> getProductByCID(String cid) {
+        List<Product> list = new ArrayList<>();
+        String query = "select * from product\n"
+                + "where cateID = ?";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, cid);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                list.add(new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6)));
+            }
+        } catch (Exception e) {
+        }
+        return list;
+    }
+         public Product getProductByID(String id) {
+        String query = "select * from product\n"
+                + "where id = ?";
+        try {
+            conn = new DBContext().getConnection();//mo ket noi voi sql
+            ps = conn.prepareStatement(query);
+            ps.setString(1, id);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                return new Product(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getDouble(4),
+                        rs.getString(5),
+                        rs.getString(6));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
         DAO dao = new DAO();
@@ -89,5 +130,7 @@ public class DAO {
             System.out.println(o);
         }
     }
+
+   
 
 }
---
+++ web/Left.jsp
@@ -0,0 +1,28 @@
+<%-- 
+    Document   : Left
+    Created on : 06-03-2022, 21:58:57
+    Author     : trieu
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+ <div class="col-sm-3">
+                    <div class="card bg-light mb-3">
+                        <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-list"></i> Danh mục đồ ăn</div>
+                        <ul class="list-group category_block">
+                            <c:forEach items="${listC}" var="o">
+                                <li class="list-group-item text-white"><a href="category?cid=${o.cid}">${o.cname}</a></li>
+                            </c:forEach>
+
+                        </ul>
+                    </div>
+                    <div class="card bg-light mb-3">
+                        <div class="card-header bg-success text-white text-uppercase">Mới nhất</div>
+                        <div class="card-body">
+                            <img class="img-fluid" src="${p.image}" />
+                            <h5 class="card-title">${p.name}</h5>
+                            <p class="card-text">${p.title}</p>
+                            <p class="bloc_left_price">${p.price} $</p>
+                        </div>
+                    </div>
+                </div>
