534367676511a208728d95426ca1b95a0513c55e
trieunvhe151319 <97203188+trieunvhe151319@users.noreply.github.com>
2022-03-12 20:58:46 +0700

    update signup
    update chuc nang signup

--- src/java/dao/DAO.java
+++ src/java/dao/DAO.java
@@ -163,14 +163,37 @@ public class DAO {
         }
         return null;
     }
-
-    public static void main(String[] args) {
-        DAO dao = new DAO();
-        List<Product> list = dao.getAllProduct();
-        List<Category> listC = dao.getAllCategory();
-
-        for (Category o : listC) {
-            System.out.println(o);
+    public Account checkAccountExist(String user){
+        String query ="select * from Account\n"
+                      + "where [user] = ?\n";
+                      
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+           
+            rs = ps.executeQuery();
+            while(rs.next()){
+                return new Account(rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getInt(5));
+            }
+        } catch (Exception e) {
+        }
+        return null;
+    }
+    public void signup(String user, String pass){
+        String query = "insert into Account\n" 
+                     +   "values(?,?,0,0)";
+        try {
+            conn = new DBContext().getConnection();
+            ps = conn.prepareStatement(query);
+            ps.setString(1, user);
+            ps.setString(1, pass);
+            ps.executeUpdate();
+        } catch (Exception e) {
         }
     }
 
